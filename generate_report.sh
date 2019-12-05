ab6daa79a507200e29d5b501d101ac757f43ddef
Russ MacShane <rmacshane@liquidweb.com>
2019-12-05 15:10:18 -0600

    initial commit

---
+++ generate_report.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+
+source ./config
+
+echo "************ Download Manifest File *********************"
+aws --endpoint=$endpoint s3 cp s3://$bucket_name/$manifest_file .
+
+echo ""
+echo "************* Plugin Report *****************************"
+cd $public_html
+wp plugin list --status=active --fields=name,version --format=json > $plugin_updater/$list_file
+cd $plugin_updater
+
+rpt_array=()
+
+for row in $(jq -r '.[] | @base64' $list_file); do
+   _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+   }
+
+   name=$(_jq '.name')
+   zip=".zip"
+   new_version=$(_jq '.version')
+   curr_version=$(jq -r --arg name "$name$zip" '.[] | select(.plugin==$name) | .version' $manifest_file)
+
+   if [ ! -z $curr_version ] && [ $new_version != $curr_version ]
+   then
+      rpt_obj=$(jq -n --arg name "$name" \
+                      --arg new_version "$new_version" \
+                      --arg curr_version "$curr_version" \
+                      '{plugin: $name, new_version: $new_version, current_version: $curr_version}')
+      rpt_array+=("$rpt_obj")
+   fi
+done
+
+length=${#rpt_array[@]}
+current=0
+echo "[" >> $report_file
+for i in "${rpt_array[@]}"
+do
+   current=$((current + 1))
+   echo "$i" >> $report_file
+   if [[ $current -ne $length ]]
+   then
+      echo "," >> $report_file
+   fi
+done
+echo "]" >> $report_file
+
+jq '.[]' $report_file
+
+echo ""
+echo "************ Zip up Plugins *****************************"
+for row in $(jq -r '.[] | @base64' $report_file); do
+   _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+   }
+
+   name=$(_jq '.plugin')
+   
+   zip $name.zip $plugins_home/$name
+done
---
+++ update_plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ./config
+
+echo "************ Cleanup Files *****************************"
+rm $report_file
+rm $manifest_file
+rm $list_file
+rm *.zip
