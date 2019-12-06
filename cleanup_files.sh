848d85fa21b06c9bb0d83233802fc46457adf6f6
Russ MacShane <rmacshane@liquidweb.com>
2019-12-06 10:23:17 -0600

    Added manifest file manipulation and cleanup files script

---
+++ cleanup_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source ./config
+echo "************ Cleanup Files *****************************"
+rm $report_file
+rm $manifest_file
+rm $list_file
+rm *.zip
--- update_plugins.sh
+++ update_plugins.sh
@@ -2,6 +2,24 @@
 
 source ./config
 
+echo "************ Update Manifest ***************************"
+for row in $(jq -r '.[] | @base64' $report_file); do
+   _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+   }
+
+   name=$(_jq '.plugin')
+   zip=".zip"
+   new_version=$(_jq '.new_version')
+
+   tmp=$(mktemp)
+   $(jq -r --arg name "$name$zip" \
+           --arg version "$new_version" \
+           '(.[] | select(.plugin==$name) | .version) |= $version' \
+           $manifest_file > "$tmp" && mv "$tmp" $manifest_file
+   )
+done
+
 echo "************ Cleanup Files *****************************"
 rm $report_file
 rm $manifest_file
